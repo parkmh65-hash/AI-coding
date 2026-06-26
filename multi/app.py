f7c9c4509359ebf403d0a61952567467fc80edf8
parkmh65-hash <parkmh65@gmail.com>
2026-06-26 21:40:56 +0900

    Change import statements to relative imports

--- multi/app.py
+++ multi/app.py
@@ -6,8 +6,8 @@ from langchain_core.messages import (
     SystemMessage
 )
 
-from graph import graph
-from utils import (
+from .graph import graph
+from .utils import (
     load_state,
     save_state
 )
