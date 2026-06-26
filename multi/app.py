c4d011d81a8a3cea7e6ad7a41cd1146608d3360a
parkmh65-hash <parkmh65@gmail.com>
2026-06-26 22:55:40 +0900

    Update app.py

--- multi/app.py
+++ multi/app.py
@@ -13,7 +13,12 @@ from .utils import (
 )
 
 from datetime import datetime
+import os
+import uvicorn
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 app = FastAPI(
     title="Book Writer Agent"
