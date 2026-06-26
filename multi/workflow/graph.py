7820309524227e07d0fa628772bb25bb699af5f2
parkmh65-hash <parkmh65@gmail.com>
2026-06-26 15:33:41 +0900

    Update graph.py

--- multi/workflow/graph.py
+++ multi/workflow/graph.py
@@ -1,1 +1,60 @@
+from langgraph.graph import (
+    StateGraph,
+    START,
+    END
+)
 
+
+from state import State
+from agents import (
+    content_strategist,
+    communicator
+)
+
+
+builder=StateGraph(State)
+
+
+
+def strategist_node(state):
+
+    return content_strategist(
+        state,
+        state["thread_id"]
+    )
+
+
+
+builder.add_node(
+    "strategist",
+    strategist_node
+)
+
+
+builder.add_node(
+    "communicator",
+    communicator
+)
+
+
+
+builder.add_edge(
+    START,
+    "strategist"
+)
+
+
+builder.add_edge(
+    "strategist",
+    "communicator"
+)
+
+
+builder.add_edge(
+    "communicator",
+    END
+)
+
+
+
+graph=builder.compile()
